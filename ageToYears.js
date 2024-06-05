6a4325a1b53aed53c80d8103ce276fbbb578a530
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-05 09:39:13 +0200

    task3 with html

--- ageToYears.js
+++ ageToYears.js
@@ -1,6 +1,14 @@
-function ageToYears(age) {
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+let display = document.querySelector('h1')
+
+button.addEventListener('click', ()=>{
+ageToDays(input.value)
+})
+
+function ageToDays(age) {
     ageInDays = age*365
     console.log(ageInDays)
+    display.innerText = 'You are ' +ageInDays + ' days old.'
 }
-ageToYears(2)
 
